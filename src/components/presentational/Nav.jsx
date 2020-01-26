f184c5c7237a7b11ade86dbeadef8a65447f18f2
milanterhes <terhes.milan@gmail.com>
2020-01-26 19:07:21 +0100

    added back button to nav

--- src/components/presentational/Nav.jsx
+++ src/components/presentational/Nav.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { useLocation, Link } from "react-router-dom";
 
 const NavBG = styled.header`
   top: 0;
@@ -12,8 +13,42 @@ const NavBG = styled.header`
   justify-content: center;
   align-items: center;
   font-size: 200%;
+  position: relative;
+`;
+
+const BackButton = styled.div`
+  width: 100px;
+  height: 50px;
+  text-align: center;
+  left: 10px;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  border: 1px solid white;
+  border-radius: 5px;
+  transition: 0.6s;
+  &:hover {
+    box-shadow: 2px 2px white;
+  }
+  a {
+    text-decoration: none;
+    color: white;
+  }
 `;
 
 export default function Nav() {
-  return <NavBG>Products</NavBG>;
+  let location = useLocation();
+  console.log(location);
+  return (
+    <NavBG>
+      {location.pathname !== "/" && (
+        <BackButton>
+          <Link to="/">Back</Link>
+        </BackButton>
+      )}
+      Products
+    </NavBG>
+  );
 }
